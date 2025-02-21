d247befef294780b1903ab4de06ce82a3adf791e
Dani <danigd71@gmail.com>
2025-02-21 11:00:31 +0100

    Nuevo componente SPINNER

--- frontend/src/app/pages/centro-ayuda/centro-ayuda.page.ts
+++ frontend/src/app/pages/centro-ayuda/centro-ayuda.page.ts
@@ -8,13 +8,14 @@ import { RouterModule } from '@angular/router';
 import { MatDivider } from '@angular/material/divider';
 import { Usuario } from 'src/app/models/user/usuario.model';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
+import { SpinnerComponent } from "../../components/spinner/spinner/spinner.component";
 
 @Component({
   selector: 'app-centro-ayuda',
   templateUrl: './centro-ayuda.page.html',
   styleUrls: ['./centro-ayuda.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule, IonicModule, MatButtonModule, RouterModule, MatDivider, NavbarComponent]
+  imports: [CommonModule, FormsModule, MatIconModule, IonicModule, MatButtonModule, RouterModule, MatDivider, NavbarComponent, SpinnerComponent]
 })
 export class CentroAyudaPage implements OnInit {
   
