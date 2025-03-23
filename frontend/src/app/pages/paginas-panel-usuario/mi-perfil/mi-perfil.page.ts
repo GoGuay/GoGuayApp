e319a572faff784be4be4ef5c7b0e13918cfc115
Juan Tomas <juan.tomas88@hotmail.com>
2025-03-23 14:04:05 +0100

    pagina mi-perfil ok

--- frontend/src/app/pages/paginas-panel-usuario/mi-perfil/mi-perfil.page.ts
+++ frontend/src/app/pages/paginas-panel-usuario/mi-perfil/mi-perfil.page.ts
@@ -35,6 +35,15 @@ export class MiPerfilPage implements OnInit {
   edad: number = this.funcionesComunes.calcularEdad(this.fechaNacimiento);
   editandoVehiculo: boolean = false;
   datosActualizados: any = {};
+  nombreEditado: string = '';
+  apellidosEditados: string = '';
+  pronombreEditado: string = '';
+  generoEditado: string = '';
+  orientacionEditada: string = '';
+  fechaNacimientoEditada: string = '';
+  bioEditada: string = '';
+  preferenciasSeleccionadas: string[] = [];
+  usuario: Usuario = {} as Usuario;
 
   constructor(
     public funcionesComunes: FuncionesComunes,
@@ -45,16 +54,16 @@ export class MiPerfilPage implements OnInit {
     this.userData = JSON.parse(localStorage.getItem('userData') || '{}');
     this.userLoggedIn = !!(this.userData && this.userData.usuario.email);
     console.log('Datos usuario: ', this.userData);
+    this.obtenerUsuario();
 
-    // Si el usuario no tiene un género guardado, asignar vacío ("")
-    // if (!this.userData.usuario.genero) {
-    //   this.userData.usuario.genero = '';
-    // }
-
-    /// Si el usuario no tiene un pronombre guardado, asignar vacío ("")
-    // if (!this.userData.usuario.pronombre) {
-    //   this.userData.usuario.pronombre = '';
-    // }
+    this.nombreEditado = this.userData.usuario.nombre;
+    this.apellidosEditados = this.userData.usuario?.apellidos;
+    this.pronombreEditado = this.userData.usuario?.pronombre || '';
+    this.generoEditado = this.userData.usuario.genero || '';
+    this.orientacionEditada = this.userData.usuario.orientacion || '';
+    this.fechaNacimientoEditada = this.userData.usuario.fecha_nacimiento || '';
+    this.bioEditada = this.userData.usuario.biografia || '';
+    this.preferenciasSeleccionadas = this.userData.usuario.preferencias || [];
 
     if (this.userData.usuario.fecha_nacimiento) {
       this.fechaNacimiento = this.userData.usuario.fecha_nacimiento;
@@ -73,6 +82,14 @@ export class MiPerfilPage implements OnInit {
     this.edad = this.funcionesComunes.calcularEdad(this.fechaNacimiento);
   }
 
+  obtenerUsuario() {
+    this.userService
+      .obtenerUsuarioPorID(this.userData.usuario.id)
+      .subscribe((respuesta) => {
+        this.usuario = respuesta;
+      });
+  }
+
   editarDatos() {
     if (
       !this.userData.usuario.nombre ||
@@ -84,16 +101,50 @@ export class MiPerfilPage implements OnInit {
       console.log('Falta algún dato obligatorio');
       return;
     }
+
+    const nuevoUsuario = {
+      nombre: this.nombreEditado,
+      apellidos: this.apellidosEditados,
+      pronombre: this.pronombreEditado,
+      genero: this.generoEditado,
+      orientacion: this.orientacionEditada,
+      fecha_nacimiento: this.fechaNacimientoEditada,
+      biografia: this.bioEditada,
+      preferencias: this.preferenciasSeleccionadas,
+    };
+    console.log('Objeto modificado: ', nuevoUsuario);
+
     this.userService
-      .editarDatosUsuario(this.userData.usuario.id, this.userData)
+      .editarDatosUsuario(this.userData.usuario.id, nuevoUsuario)
       .subscribe(
         (response) => {
           console.log('Datos actualizado con exito', response);
-          localStorage.setItem('userData', JSON.stringify(response));
+          this.userData.usuario = { ...this.userData.usuario, ...nuevoUsuario };
+          localStorage.setItem('userData', JSON.stringify(this.userData));
+          this.obtenerUsuario();
         },
         (error) => {
           console.error('Error al actualizar los datos', error);
         }
       );
+  }
+
+  actualizarPreferencias(event: any) {
+    const valor = event.target.value;
+
+    if (!Array.isArray(this.preferenciasSeleccionadas)) {
+      this.preferenciasSeleccionadas = [];
+    }
+    if (event.target.checked) {
+      if (!this.preferenciasSeleccionadas.includes(valor)) {
+        this.preferenciasSeleccionadas.push(valor);
+      }
+    } else {
+      this.preferenciasSeleccionadas = this.preferenciasSeleccionadas.filter(
+        (pref) => pref !== valor
+      );
+    }
+
+    console.log('Preferencias actualizadas:', this.preferenciasSeleccionadas);
   }
 }
