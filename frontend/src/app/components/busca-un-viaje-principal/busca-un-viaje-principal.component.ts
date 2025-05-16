cd0118378436e176a5e8306c19166e58ab2d3ac5
Dani <danigd71@gmail.com>
2025-05-16 21:49:22 +0200

    Ajustes visuales

--- frontend/src/app/components/busca-un-viaje-principal/busca-un-viaje-principal.component.ts
+++ frontend/src/app/components/busca-un-viaje-principal/busca-un-viaje-principal.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-busca-un-viaje-principal',
   standalone: true,
-  imports: [IonicModule, MatButtonModule, TranslateModule, MatIcon],
+  imports: [IonicModule, MatButtonModule, TranslateModule],
   templateUrl: './busca-un-viaje-principal.component.html',
   styleUrls: ['./busca-un-viaje-principal.component.scss'],
 })
