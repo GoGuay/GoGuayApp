21f4964a6d9ce673b6fa55cb658892a046feb1f9
Juan Tomas <juan.tomas88@hotmail.com>
2025-05-24 22:58:22 +0200

    input para el correo electronico

--- frontend/src/app/pages/forgot-password/forgot-password.page.ts
+++ frontend/src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCol,
+  IonRow,
+} from '@ionic/angular/standalone';
+import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonRow,
+    IonCol,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    NavbarComponent,
+    TranslateModule,
+  ],
 })
 export class ForgotPasswordPage implements OnInit {
+  userLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
