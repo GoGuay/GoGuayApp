6bc4bebf35c5ea543ee3d2adf3ef53aae00fb60a
Juan Tomas <juan.tomas88@hotmail.com>
2025-05-26 18:51:25 +0200

    Función para que la contraseña cumpla ciertas características y coincida en los 2 campos.

--- frontend/src/app/pages/nueva-contrasena/nueva-contrasena.page.ts
+++ frontend/src/app/pages/nueva-contrasena/nueva-contrasena.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  Validators,
+  FormGroup,
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import {
   IonContent,
   IonHeader,
@@ -12,6 +20,24 @@ import {
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDivider } from '@angular/material/divider';
+
+/**
+ * Valida si las 2 contraseñas introducidas son iguales
+ * Recibe el formulario completo y extrae los 2 valores: nuevaPassword1 y nuevaPassword2
+ * Si no son iguales retorna un objeto de error {noCoinciden: true}
+ * Si coinciden, retorna null.
+ * AbstractControl es el tipo de dato que representa el formulario completo.
+ * ValidationErrors indica que puede devolver errores o null.
+ * @param group
+ * @returns
+ */
+function passwordsCoincidentes(
+  group: AbstractControl
+): ValidationErrors | null {
+  const pass1 = group.get('nuevaPassword1')?.value;
+  const pass2 = group.get('nuevaPassword2')?.value;
+  return pass1 && pass2 && pass1 !== pass2 ? { noCoinciden: true } : null;
+}
 
 @Component({
   selector: 'app-nueva-contrasena',
@@ -27,11 +53,42 @@ import { MatDivider } from '@angular/material/divider';
     NavbarComponent,
     TranslateModule,
     MatDivider,
+    ReactiveFormsModule,
   ],
 })
 export class NuevaContrasenaPage implements OnInit {
   userLoggedIn: boolean = false;
-  constructor() {}
+  formulario: any;
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group(
+      {
+        nuevaPassword1: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern('^(?=.*[A-Z])(?=.*[\\d\\W]).{6,}$'),
+          ],
+        ],
+        nuevaPassword2: [{ value: '', disabled: true }, Validators.required],
+      },
+      {
+        validators: passwordsCoincidentes,
+      }
+    );
+  }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.formulario
+      .get('nuevaPassword1')
+      ?.statusChanges.subscribe((status: string) => {
+        const pass2 = this.formulario.get('nuevaPassword2');
+        if (status === 'VALID') {
+          pass2?.enable();
+        } else {
+          pass2?.disable();
+          pass2?.reset();
+        }
+      });
+  }
 }
