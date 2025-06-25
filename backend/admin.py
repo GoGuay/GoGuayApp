d0f0ce03678a475ea1db4af43a7906bf471b169e
Dani <danigd71@gmail.com>
2025-06-25 13:17:46 +0200

    NUEVO: Conexión global a BASE DE DATOS

--- backend/admin.py
+++ backend/admin.py
@@ -3,7 +3,7 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from extensions import db
 from models import Usuario, Vehiculo, Monedero, Puntuacion, Viaje, Notificacion
-from views.HomeView import HomeView  # Cambia esta línea para la nueva ubicación
+from views.HomeView import HomeView 
  
 def setup_admin(app):
     admin = Admin(app, name='PRIDERIDE BD', template_mode='bootstrap4')
