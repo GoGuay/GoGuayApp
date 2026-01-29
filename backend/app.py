89bc038c61f76cda1bebac08a3429d4977a190ea
Dani <danigd71@gmail.com>
2026-01-29 13:31:02 +0100

    nueva configuración para despliegue en vercel

--- backend/app.py
+++ backend/app.py
@@ -43,7 +43,7 @@ def create_app():
     app.config.from_object(Config)
     JWTManager(app)
     
-    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:4200", "https://pride-ride.vercel.app", "https://pride-ride-4tpkx55u8-priderides-projects.vercel.app"], "supports_credentials": True}})
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:4200", "https://pride-ride.vercel.app", "https://pride-ride-4tpkx55u8-priderides-projects.vercel.app"], "supports_credentials": True}})
 
     db.init_app(app)
     
