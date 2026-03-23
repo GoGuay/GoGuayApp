21442c94602463ae05779ad100f998b47be6963f
Juan Tomas <juan.tomas88@hotmail.com>
2026-03-23 22:03:15 +0100

    FIX: Accesibilidad y funcionalidad de los inputs de origen y destino.
    Se permite el paso de un input a otro cuando no hay ningún dato, pero si la localidad no se encuentra entre el listado, se borra el input dejando el foco en ese mismo input.

--- frontend/src/app/components/buscador/buscador.component.ts
+++ frontend/src/app/components/buscador/buscador.component.ts
@@ -25,8 +25,9 @@ import {
   distinctUntilChanged,
   filter,
   switchMap,
+  tap,
 } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-buscador',
@@ -59,6 +60,13 @@ export class BuscadorComponent implements OnInit {
   sugerenciasOrigen: any[] = [];
   sugerenciasDestino: any[] = [];
 
+  // Índices para el control de teclado
+  indiceActivoOrigen: number = -1;
+  indiceActivoDestino: number = -1;
+
+  private ultimaLocalidadValidaOrigen: any = null;
+  private ultimaLocalidadValidaDestino: any = null;
+
   @ViewChild('inputOrigen') inputOrigen!: ElementRef;
   @ViewChild('inputDestino') inputDestino!: ElementRef;
 
@@ -66,6 +74,12 @@ export class BuscadorComponent implements OnInit {
 
   private buscadorOrigen$ = new Subject<string>();
   private buscadorDestino$ = new Subject<string>();
+
+  //Variable para guardar una búsqueda en caché. Para saber si ya ha sido buscada previamente o no.
+  private cacheConsultas: { [key: string]: any[] } = {};
+
+  estaEnOrigen: boolean = false;
+  estaEnDestino: boolean = false;
 
   constructor(
     public translate: TranslateService,
@@ -87,30 +101,43 @@ export class BuscadorComponent implements OnInit {
       .pipe(
         debounceTime(400),
         distinctUntilChanged(),
+        filter(() => this.estaEnOrigen),
         filter((texto) => {
           const regexLetra = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]/;
           return regexLetra.test(texto);
         }),
         switchMap((texto) => {
+          const termino = texto.toLowerCase().trim();
+          if (this.cacheConsultas[termino]) {
+            return of(this.cacheConsultas[termino]);
+          }
           if (texto.length >= 3) {
-            return this.googleService.obtenerLocalidad(texto);
+            return this.googleService
+              .obtenerLocalidad(texto)
+              .pipe(
+                tap(
+                  (resultados) => (this.cacheConsultas[termino] = resultados),
+                ),
+              );
           } else {
             this.sugerenciasOrigen = [];
             return [];
           }
         }),
+        filter(() => this.estaEnOrigen),
       )
       .subscribe((respuesta: any) => {
         this.sugerenciasOrigen = respuesta;
       });
 
     /**
-     * CEREBRO BUSQUEDA LOCALIDAD DESTINO
+     * CEREBRO BUSQUEDA LOCALIDAD DESTINO: Funciona igual que la de origen
      */
     this.buscadorDestino$
       .pipe(
         debounceTime(400),
         distinctUntilChanged(),
+        filter(() => this.estaEnDestino),
         filter((texto) => {
           const regexLetra = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]/;
           return regexLetra.test(texto);
@@ -123,6 +150,7 @@ export class BuscadorComponent implements OnInit {
             return [];
           }
         }),
+        filter(() => this.estaEnDestino),
       )
       .subscribe((respuesta: any) => {
         this.sugerenciasDestino = respuesta;
@@ -179,6 +207,10 @@ export class BuscadorComponent implements OnInit {
   }
 
   /**
+   * INTERACCIÓN DE BÚSQUEDA Y TECLADO
+   */
+
+  /**
    * Obtiene una lista de sugerencias de búsqueda en la ciudad de origen.
    * @param evento
    */
@@ -202,7 +234,10 @@ export class BuscadorComponent implements OnInit {
    */
   seleccionarLocalidadOrigen(localidad: any) {
     this.origen = localidad.descripcion.split(',')[0].trim();
+    this.ultimaLocalidadValidaOrigen = localidad;
     this.sugerenciasOrigen = [];
+    this.indiceActivoOrigen = -1;
+    this.inputOrigen.nativeElement.focus();
   }
 
   /**
@@ -211,7 +246,112 @@ export class BuscadorComponent implements OnInit {
    */
   seleccionarLocalidadDestino(localidad: any) {
     this.destino = localidad.descripcion.split(',')[0].trim();
+    this.ultimaLocalidadValidaDestino = localidad;
     this.sugerenciasDestino = [];
+    this.indiceActivoDestino = -1;
+    this.inputDestino.nativeElement.focus();
+  }
+
+  /**
+   *
+   * @param event --> información de la tecla pulsada (flecha abajo, Esc, etc)
+   * @param tipo --> para saber si estamos trabajando con el input de 'origen' o 'destino'.
+   * @param index --> si es -1 el usuario pulsó la tecla estando dentro del input. Si es 0,1,2...significa que el usuario ya esta navegando en la lista de sugerencias.
+   *
+   * Condicional sugerencias: si el tipo es origen, elige sugerenciasOrigen. Si no es ese tipo, coge sugerenciasDestino. Si el array de sugerencias es 0 sale de la función.
+   * Condicional indiceActual: si el tipo es origen indiceActual pasa a valer lo que esté en la definición de indiceActivoOrigen (-1), si no pasa a valor lo que tenga indiceActivoDestino (-1).
+   * Si el evento es tecla abajo:
+   *  -event.preventDefault --> indicamos que somos nosotros quienes vamos a manejar con la tecla, impedimos la accion natural que tiene el navegador.
+   *  - Si indiceActual es menor que el array de sugerencias -1 (para igual el tamaño del array al número del índice), le sumamos 1 indiceActual y llamamos a actualizarIndiceyFoco
+   * Si el evento es tecla arriba:
+   *  -
+   */
+  manejarNavegacionTeclado(
+    event: KeyboardEvent,
+    tipo: 'origen' | 'destino',
+    index: number = -1,
+  ) {
+    const sugerencias =
+      tipo === 'origen' ? this.sugerenciasOrigen : this.sugerenciasDestino;
+    let indiceActual =
+      tipo === 'origen' ? this.indiceActivoOrigen : this.indiceActivoDestino;
+
+    if (sugerencias.length === 0) return;
+
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      if (indiceActual < sugerencias.length - 1) {
+        indiceActual++;
+        this.actualizarIndiceYFoco(tipo, indiceActual);
+      }
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      if (indiceActual > 0) {
+        indiceActual--;
+        this.actualizarIndiceYFoco(tipo, indiceActual);
+      } else {
+        this.actualizarIndiceYFoco(tipo, -1);
+        const input = tipo === 'origen' ? this.inputOrigen : this.inputDestino;
+        input.nativeElement.focus();
+      }
+    } else if (event.key === 'Enter') {
+      // Si hay algo seleccionado en la lista, lo elegimos
+      if (indiceActual !== -1) {
+        event.preventDefault();
+        const seleccionada = sugerencias[indiceActual];
+        if (tipo === 'origen') {
+          this.seleccionarLocalidadOrigen(seleccionada);
+        } else {
+          this.seleccionarLocalidadDestino(seleccionada);
+        }
+      }
+    } else if (event.key === 'Escape') {
+      this.limpiarSugerencias(tipo);
+    }
+  }
+
+  private actualizarIndiceYFoco(
+    tipo: 'origen' | 'destino',
+    nuevoIndice: number,
+  ) {
+    if (tipo === 'origen') {
+      this.indiceActivoOrigen = nuevoIndice;
+    } else {
+      this.indiceActivoDestino = nuevoIndice;
+    }
+
+    if (nuevoIndice !== -1) {
+      // Selector dinámico basado en el tipo
+      const selector =
+        tipo === 'origen'
+          ? '.lista_sugerencias_origen'
+          : '.lista_sugerencias_destino';
+      setTimeout(() => {
+        const elementos = document.querySelectorAll(selector);
+        (elementos[nuevoIndice] as HTMLElement)?.focus();
+      }, 10);
+    }
+  }
+
+  limpiarSugerencias(
+    tipo: 'origen' | 'destino',
+    devolverFoco: boolean = false,
+  ) {
+    if (tipo === 'origen') {
+      this.sugerenciasOrigen = [];
+      this.indiceActivoOrigen = -1;
+      this.buscadorOrigen$.next('');
+      if (devolverFoco) {
+        this.inputOrigen.nativeElement.focus();
+      }
+    } else {
+      this.sugerenciasDestino = [];
+      this.indiceActivoDestino = -1;
+      this.buscadorDestino$.next('');
+      if (devolverFoco) {
+        this.inputDestino.nativeElement.focus();
+      }
+    }
   }
 
   buscar() {
@@ -234,5 +374,45 @@ export class BuscadorComponent implements OnInit {
 
     this.onSearch.emit(params);
     this.navCtrl.navigateForward('/busqueda-viajes', { queryParams: params });
+  }
+
+  validarSeleccion(tipo: 'origen' | 'destino') {
+    setTimeout(() => {
+      if (tipo === 'origen') {
+        const textoActual = this.origen.trim();
+        const textoValido = this.ultimaLocalidadValidaOrigen?.descripcion
+          .split(',')[0]
+          .trim();
+
+        if (textoActual === '') {
+          this.ultimaLocalidadValidaOrigen = null;
+          this.limpiarSugerencias('origen', false);
+          return;
+        }
+
+        if (!this.ultimaLocalidadValidaOrigen || textoActual !== textoValido) {
+          this.origen = '';
+          this.ultimaLocalidadValidaOrigen = null;
+          this.limpiarSugerencias('origen', true);
+        }
+      } else {
+        const textoActual = this.destino.trim();
+
+        if (textoActual === '') {
+          this.ultimaLocalidadValidaDestino = null;
+          this.limpiarSugerencias('destino', false);
+          return;
+        }
+        const textoValido = this.ultimaLocalidadValidaDestino?.descripcion
+          .split(',')[0]
+          .trim();
+
+        if (!this.ultimaLocalidadValidaDestino || textoActual !== textoValido) {
+          this.destino = '';
+          this.ultimaLocalidadValidaDestino = null;
+          this.limpiarSugerencias('destino', true);
+        }
+      }
+    }, 250);
   }
 }
